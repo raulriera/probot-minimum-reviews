de4e1c07e53b1b7bb22a895062c514d0a59e0019
Raul Riera <rieraraul@gmail.com>
2018-04-09 08:05:57 -0400

    Fix 'standard' errors

--- test/index.test.js
+++ test/index.test.js
@@ -28,7 +28,7 @@ describe('your-app', () => {
     github = {
       pullRequests: {
         getReviews: jest.fn().mockReturnValue(Promise.resolve({
-          data: [{ state: "APPROVED" }]
+          data: [{ state: 'APPROVED' }]
         }))
       },
       repos: {
@@ -49,32 +49,30 @@ describe('your-app', () => {
   describe('your functionality', () => {
     it('performs an action', async () => {
       const payloadFixture = {
-        event: "pull_request",
+        event: 'pull_request',
         payload: payload
       }
 
       // Simulates delivery of a payload
-      // payload.event is the X-GitHub-Event header sent by GitHub (for example "push")
+      // payload.event is the X-GitHub-Event header sent by GitHub (for example 'push')
       // payload.payload is the actual payload body
       await robot.receive(payloadFixture)
 
-      //console.log(payload)
-
       expect(github.pullRequests.getReviews).toHaveBeenCalledWith({
-        "number": 1,
-        "owner": "raulriera",
-        "repo": "probot-test"
+        'number': 1,
+        'owner': 'raulriera',
+        'repo': 'probot-test'
       })
 
       // This test would pass if in your main code you called `github.repos.createStatus`
       expect(github.repos.createStatus).toHaveBeenCalledWith({
-        "context": "probot/minimum-reviews",
-        "description": "No pending reviews",
-        "owner": "raulriera",
-        "repo": "probot-test",
-        "sha": "e7a3abf45bec74b74fc71d4a653a0e6c754e572a",
-        "state": "success",
-        "target_url": "https://github.com/apps/minimum-reviews"
+        'context': 'probot/minimum-reviews',
+        'description': 'No pending reviews',
+        'owner': 'raulriera',
+        'repo': 'probot-test',
+        'sha': 'e7a3abf45bec74b74fc71d4a653a0e6c754e572a',
+        'state': 'success',
+        'target_url': 'https://github.com/apps/minimum-reviews'
       })
     })
   })
